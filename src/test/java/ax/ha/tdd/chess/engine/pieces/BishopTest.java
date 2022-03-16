0c14c409ab08c20ac8da26882105479e1b6455de
Christian Syväluoma <christian.syvaluoma@ha.ax>
2022-03-16 14:30:51 +0200

    Bishop capture enemy and no capture of friendly

--- src/test/java/ax/ha/tdd/chess/engine/pieces/BishopTest.java
+++ src/test/java/ax/ha/tdd/chess/engine/pieces/BishopTest.java
@@ -30,4 +30,22 @@ class BishopTest {
         board.movePiece("c1-g8",Player.WHITE);
         assertEquals(new Coordinates("c1"),whiteBishop.getLocation());
     }
+    @Test
+    void captureEnemyPiece() {
+        Bishop whiteBishop = new Bishop(PieceType.BISHOP, Player.WHITE, new Coordinates("c1"));
+        Pawn blackPawn = new Pawn(PieceType.PAWN, Player.BLACK, new Coordinates("f4"));
+        board.addPiece(whiteBishop);
+        board.addPiece(blackPawn);
+        board.movePiece("c1-f4",Player.WHITE);
+        assertEquals(new Coordinates("f4"),whiteBishop.getLocation());
+    }
+    @Test
+    void noCaptureOwnPiece() {
+        Bishop whiteBishop = new Bishop(PieceType.BISHOP, Player.WHITE, new Coordinates("c1"));
+        Pawn whitePawn = new Pawn(PieceType.PAWN, Player.WHITE, new Coordinates("f4"));
+        board.addPiece(whiteBishop);
+        board.addPiece(whitePawn);
+        board.movePiece("c1-f4",Player.WHITE);
+        assertEquals(new Coordinates("c1"),whiteBishop.getLocation());
+    }
 }
