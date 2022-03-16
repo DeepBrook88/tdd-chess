1faab56a7b45885feadd99a477aac37556880f94
Christian Syväluoma <christian.syvaluoma@ha.ax>
2022-03-16 14:59:53 +0200

    valid and invalid movements tests

---
+++ src/main/java/ax/ha/tdd/chess/engine/pieces/Queen.java
@@ -0,0 +1,33 @@
+package ax.ha.tdd.chess.engine.pieces;
+
+import ax.ha.tdd.chess.engine.Chessboard;
+import ax.ha.tdd.chess.engine.Coordinates;
+import ax.ha.tdd.chess.engine.Player;
+
+public class Queen extends ChessPiece{
+    public Queen(PieceType pieceType, Player player, Coordinates location) {
+        super(pieceType, player, location);
+    }
+
+    @Override
+    public String getSymbol() {
+        return pieceType.getSymbol();
+    }
+
+    @Override
+    public boolean move(Chessboard chessboard, Coordinates destination) {
+        if (canMove(chessboard, destination)) {
+            location = destination;
+            hasMoved = true;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean canMove(Chessboard chessboard, Coordinates destination) {
+        Bishop b = new Bishop(this.pieceType, this.player, this.location);
+        Rook r = new Rook(this.pieceType, this.player, this.location);
+        return b.canMove(chessboard,destination) || r.canMove(chessboard,destination);
+    }
+}
---
+++ src/test/java/ax/ha/tdd/chess/engine/pieces/QueenTest.java
@@ -0,0 +1,42 @@
+package ax.ha.tdd.chess.engine.pieces;
+
+import ax.ha.tdd.chess.engine.Chessboard;
+import ax.ha.tdd.chess.engine.Coordinates;
+import ax.ha.tdd.chess.engine.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueenTest {
+    private Chessboard board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Chessboard();
+    }
+
+    @Test
+    void canMoveLikeBishop() {
+        Queen whiteQueen = new Queen(PieceType.QUEEN, Player.WHITE, new Coordinates("d1"));
+        board.addPiece(whiteQueen);
+        board.movePiece("d1-g4",Player.WHITE);
+        assertEquals(new Coordinates("g4"),whiteQueen.getLocation());
+    }
+
+    @Test
+    void canMoveLikeRook() {
+        Queen whiteQueen = new Queen(PieceType.QUEEN, Player.WHITE, new Coordinates("a1"));
+        board.addPiece(whiteQueen);
+        board.movePiece("a1-g1",Player.WHITE);
+        assertEquals(new Coordinates("g1"),whiteQueen.getLocation());
+    }
+
+    @Test
+    void invalidMovement() {
+        Queen whiteQueen = new Queen(PieceType.QUEEN, Player.WHITE, new Coordinates("a1"));
+        board.addPiece(whiteQueen);
+        board.movePiece("a1-b3",Player.WHITE);
+        assertEquals(new Coordinates("a1"),whiteQueen.getLocation());
+    }
+}
