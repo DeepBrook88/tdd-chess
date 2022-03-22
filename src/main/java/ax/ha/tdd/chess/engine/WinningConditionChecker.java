2ec7f8a4a5df6b7e1673612c09197d3e8b37c398
Christian Syväluoma <christian.syvaluoma@ha.ax>
2022-03-22 15:04:28 +0200

    check state implemented

--- src/main/java/ax/ha/tdd/chess/engine/WinningConditionChecker.java
+++ src/main/java/ax/ha/tdd/chess/engine/WinningConditionChecker.java
@@ -1,8 +1,28 @@
 package ax.ha.tdd.chess.engine;
 
+import ax.ha.tdd.chess.engine.pieces.ChessPiece;
+import ax.ha.tdd.chess.engine.pieces.PieceType;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WinningConditionChecker {
     private WinningConditionChecker(){}
     public static WinningState checkState(Chessboard chessboard, Player player) {
+        List<ChessPiece> pieces = new ArrayList<>();
+        ChessPiece king = null;
+        for (ChessPiece[] chessPieces : chessboard) {
+            for (ChessPiece chessPiece : chessPieces) {
+                if (chessPiece != null && !chessPiece.getPlayer().equals(player)
+                        && !chessPiece.getPieceType().equals(PieceType.PAWN)
+                        && !chessPiece.getPieceType().equals(PieceType.KING)) pieces.add(chessPiece);
+                if (chessPiece != null && chessPiece.getPlayer().equals(player)
+                        && chessPiece.getPieceType().equals(PieceType.KING)) king = chessPiece;
+            }
+        }
+        for (ChessPiece piece : pieces) {
+            if (king != null && piece.canThreatenKing(chessboard, king.getLocation())) return WinningState.CHECK;
+        }
         return WinningState.PLAYING;
     }
 }
--- src/test/java/ax/ha/tdd/chess/engine/WinStateCheckerTest.java
+++ src/test/java/ax/ha/tdd/chess/engine/WinStateCheckerTest.java
@@ -1,5 +1,9 @@
 package ax.ha.tdd.chess.engine;
 
+import ax.ha.tdd.chess.console.ChessboardWriter;
+import ax.ha.tdd.chess.engine.pieces.King;
+import ax.ha.tdd.chess.engine.pieces.PieceType;
+import ax.ha.tdd.chess.engine.pieces.Queen;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,5 +13,16 @@ class WinStateCheckerTest {
     void checkState_givenDefaultBoardAndBlackPlayer_expectPlaying() {
         assertEquals(WinningState.PLAYING,
                 WinningConditionChecker.checkState(Chessboard.startingBoard(), Player.BLACK));
+    }
+    @Test
+    void checkStateAndBlackPlayerExpectCheck() {
+        Chessboard board = new Chessboard();
+        King blackKing = new King(PieceType.KING, Player.BLACK, new Coordinates("e8"));
+        Queen whiteQueen = new Queen(PieceType.QUEEN, Player.WHITE, new Coordinates("d1"));
+        board.addPiece(blackKing);
+        board.addPiece(whiteQueen);
+        board.movePiece("d1-e2", Player.WHITE);
+        assertEquals(WinningState.CHECK,
+                WinningConditionChecker.checkState(board, Player.BLACK));
     }
 }
